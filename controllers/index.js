833511432cf6158918d118e86a6416f18d000f07
Alex Bardanov <dnbard@gmail.com>
2015-08-14 18:19:21 +0300

    render user id when user is authenticated

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,8 @@ exports.default = function(req, res){
     if (!req.isAuthenticated()){
         return res.render('indexDefault.jade');
     }
-    return res.render('indexAuth.jade');
+
+    return res.render('indexAuth.jade', {
+        userId: req.user._id
+    });
 }
