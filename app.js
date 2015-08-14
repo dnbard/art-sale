4c4fe67675bb3fa6de5384a0b788d2623b9e56eb
Alex Bardanov <dnbard@gmail.com>
2015-08-14 15:15:31 +0300

    recreate frontend assets on app init

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var babel = require("babel");
 
 app.engine('jade', require('jade').__express);
+
+babel.transformFile('./frontend/app.js', {}, function(err, result){
+    var fs = require('fs');
+
+    if (err){
+        return console.log(err);
+    }
+
+    fs.writeFile('public/app.js', result.code, function(err){
+        if (err){
+            return console.log(err);
+        }
+
+        console.log('Babelified: public/app.js');
+    });
+});
 
 mongoose.connect('mongodb://localhost:27017/artail', require('./core/configParser')(function(config){
     var server = app.listen(process.env.PORT || config.get('port'), function () {
